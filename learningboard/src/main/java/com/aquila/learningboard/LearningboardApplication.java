156640d53d473d8df87641ff6f19a4c0dcbc47e4
Aquila Venancio <aquila_os@hotmail.com>
2023-03-08 10:55:21 -0300

    Including the first code

---
+++ learningboard/src/main/java/com/aquila/learningboard/LearningboardApplication.java
@@ -0,0 +1,13 @@
+package com.aquila.learningboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningboardApplication.class, args);
+	}
+
+}
---
+++ learningboard/src/test/java/com/aquila/learningboard/LearningboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquila.learningboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
