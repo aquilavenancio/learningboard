e921adaf6bab26f2f19fde8eb6c210ef48a5c7af
Aquila Venancio <aquila_os@hotmail.com>
2023-03-08 21:15:28 -0300

    inclusão de dependencias do primefaces

--- learningboard/src/main/java/com/aquila/learningboard/repository/ProjectRepository.java
+++ learningboard/src/main/java/com/aquila/learningboard/repository/ProjectRepository.java
@@ -1,9 +1,19 @@
 package com.aquila.learningboard.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.aquila.learningboard.entyties.Project;
 
-public interface ProjectRepository extends JpaRepository<Long, Project>{
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long>{
     
-    
+
+    @Query("from Project pjt where pjt.startDate between :start and :end ")
+    List<Project> findBetweenDate(@Param("start") LocalDate startDate, @Param("end")LocalDate endDate);
 }
--- learningboard/src/main/java/com/aquila/learningboard/service/impl/ProjectServiceImpl.java
+++ learningboard/src/main/java/com/aquila/learningboard/service/impl/ProjectServiceImpl.java
@@ -2,10 +2,13 @@ package com.aquila.learningboard.service.impl;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.aquila.learningboard.business.ProjectBusiness;
+import com.aquila.learningboard.business.impl.BusinessException;
 import com.aquila.learningboard.entyties.Project;
 import com.aquila.learningboard.repository.ProjectRepository;
 import com.aquila.learningboard.service.ProjectService;
@@ -16,28 +19,36 @@ public class ProjectServiceImpl implements ProjectService{
     @Autowired
     private ProjectRepository repository;
 
+    @Autowired
+    private ProjectBusiness projectBusiness;
+
     @Override
     public Project save(Project project) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        if(projectBusiness.validateMandatoryFields(project)){
+            return repository.save(project);
+        }else{
+            throw new BusinessException("O preenchimento dos campos nome, descricao e data de inicio é obrigatório!");
+        }        
     }
 
     @Override
     public Project findById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Optional<Project> projectReturn = repository.findById(id);
+        return projectReturn.isPresent() ? projectReturn.get() : null;
     }
 
     @Override
     public List<Project> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        return repository.findAll();
     }
 
     @Override
     public List<Project> findBetweenDate(LocalDate startDate, LocalDate endDate) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findBetweenDate'");
+        if(startDate != null && endDate != null ){
+           return repository.findBetweenDate(startDate, endDate); 
+        }else{
+            throw new BusinessException("Nenhum registro encontrado!");
+        }
     }
     
 }
